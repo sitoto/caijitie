0b3f19bb2fda12dabc4c56243574693214f64694
sitoto <=huang.gadela@gmail.com>
2012-03-15 19:21:37 +0800

    fan dao lian

--- app/helpers/pu_helper.rb
+++ app/helpers/pu_helper.rb
@@ -1,2 +1,11 @@
 module PuHelper
+  def change_image_url str
+    reg_img = Regexp.new("<img.[^>]*src(=| )(.[^>]*)>")
+
+    mc = reg_img.match(str)
+    str.gsub(reg_img, '<img')
+    mc.each do |f|
+
+    end
+  end
 end
