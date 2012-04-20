7a051188fca00311ca2006c26d59d244646a9d8d
sitoto <=huang.gadela@gmail.com>
2012-04-20 13:38:19 +0800

    modify the nav bottom and relation link  to /p/1/pu/1 and class limit 40

--- app/controllers/class_controller.rb
+++ app/controllers/class_controller.rb
@@ -1,8 +1,8 @@
 #encoding: utf-8
 class ClassController < ApplicationController
   def index
-    @classes = Topic.find_by_sql("SELECT count(*) as count , classname  FROM `topics` GROUP BY classname  ORDER BY count DESC")
-    @authores = Topic.find_by_sql("SELECT count(*) as count, author  FROM `topics` GROUP BY author  ORDER BY count DESC")
+    @classes = Topic.find_by_sql("SELECT count(*) as count , classname  FROM `topics` GROUP BY classname  ORDER BY count DESC limit 40")
+    @authores = Topic.find_by_sql("SELECT count(*) as count, author  FROM `topics` GROUP BY author  ORDER BY count DESC limit 40")
     breadcrumb :list, '分类'
     meta :title => "分类", :description => "脱水文章分类" , :keywords => "文章,分类"
   end
