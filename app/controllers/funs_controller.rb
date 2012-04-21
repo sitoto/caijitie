c85e8b518ad12eef1a2106b9ce0f061f25679c71
sitoto <=huang.gadela@gmail.com>
2012-04-21 10:04:44 +0800

    funs 's cache test in production

--- app/controllers/funs_controller.rb
+++ app/controllers/funs_controller.rb
@@ -19,24 +19,23 @@ class FunsController < ApplicationController
   # GET /funs/1
   # GET /funs/1.json
   def show
-    a = Integer(params[:id]) -1
-    b = Integer(params[:id]) +1
-    @fun =Fun.find(params[:id])
-    @fun_p =Fun.find_by_sql("select id, title from funs where id = #{a}").first
-    @fun_n =Fun.find_by_sql("select id, title from funs where id = #{b}").first
+    unless read_fragment(:action => 'show')
+      a = Integer(params[:id]) -1
+      b = Integer(params[:id]) +1
+      @fun =Fun.find(params[:id])
+      @fun_p =Fun.find_by_sql("select id, title from funs where id = #{a}").first
+      @fun_n =Fun.find_by_sql("select id, title from funs where id = #{b}").first
 
-    @fun.increment!(:click_time, by = 1)
-    arr = get_random_numbers(Fun.count,18)
-   # @relation_funs = Fun.find_by_sql("select id, title from funs where id in (#{arr})");
-    @relation_funs = Fun.find(arr, :select => "id, title")
-    breadcrumb :fun_detail, @fun
-    meta :title =>  @fun.title ,
+      @fun.increment!(:click_time, by = 1)
+      arr = get_random_numbers(Fun.count,18)
+      # @relation_funs = Fun.find_by_sql("select id, title from funs where id in (#{arr})");
+      @relation_funs = Fun.find(arr, :select => "id, title")
+      breadcrumb :fun_detail, @fun
+      meta :title =>  @fun.title ,
          :description => ActionController::Base.helpers.strip_tags(@fun.body).truncate(80),
          :keywords => @fun.title
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @fun }
     end
+
   end
 
   def get_random_numbers  count,max
