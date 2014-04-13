3c15fcf851f5cae82681866724bb14f4eb12061b
sitoto <huang.gadela@gmail.com>
2014-04-13 13:06:11 +0800

    Fix js

--- app/helpers/ad_helper.rb
+++ app/helpers/ad_helper.rb
@@ -1,27 +1,52 @@
 #encoding: UTF-8
 module AdHelper
   def ad_mobile_baidu_line
-    '<script type="text/javascript">/*head*/var cpro_id = "u1455590";</script>
-<script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>'
+    '<script type="text/javascript">
+    if  (window.innerWidth  >= 800) {
+/*2-4_AllHead_960_60*/ var cpro_id = "u891671";
+    }
+    else {
+    /*head*/var cpro_id = "u1455590";
+}  
+    </script>
+<script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>
+<script src="http://cpro.baidu.com/cpro/ui/c.js" type="text/javascript"></script>' 
+
   end
   def ad_mobile_baidu_image_line
-'<script type="text/javascript">/*mobile_head_image_20:3，创建于2014-4-10*/var cpro_id = "u1518435";</script>
-<script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>'
+    '<script type="text/javascript">
+    if  (window.innerWidth  >= 800) {
+    /*4_Center_728_90*/ var cpro_id = "u891731";
+    }
+    else {
+    /*mobile_head_image_20:3，创建于2014-4-10*/var cpro_id = "u1518435";}  
+    </script>
+
+    <script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>
+    <script src="http://cpro.baidu.com/cpro/ui/c.js" type="text/javascript"></script>' 
   end
   def  ad_mobile_baidu_xuanfu
     '<script type="text/javascript">
     if  (window.innerWidth  >= 800) {
-/*mobile_20:3，创建于2014-4-12*/  var cpro_id = "u1520137";   }  
-    else {
 /*2-4_sides_120_270*/ var cpro_id = "u890141";}
+    else {
+/*mobile_20:3，创建于2014-4-12*/  var cpro_id = "u1520137";   }  
     </script>
 <script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>
 <script src="http://cpro.baidu.com/cpro/ui/f.js" type="text/javascript"></script> 
     '
   end
   def ad_mobile_baidu_relation
-'<script type="text/javascript">/*mobile_20:3，创建于2014-4-12*/var cpro_id = "u1520142";</script>
-<script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>'
+    '<script type="text/javascript">
+    if  (window.innerWidth  >= 800) {
+/*2-4_RelationLink_468_15*/ var cpro_id = "u891707";}
+    else {
+/*mobile_20:3，创建于2014-4-12*/var cpro_id = "u1520142";
+    }  
+    </script>
+<script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>
+<script src="http://cpro.baidu.com/cpro/ui/c.js" type="text/javascript"></script> 
+    '
   end
 
   def ad_baidu_sides
@@ -30,8 +55,10 @@ module AdHelper
 
   def ad_baidu_right_down
     '<script type="text/javascript">if  (window.innerWidth  >= 800) {
-/*mobile_20:3，创建于2014-4-12*/  var cpro_id = "u1520137";}  else {
-/*3-5_RightDown_300_250*/ var cpro_id = "u890150";    }</script> 
+/*3-5_RightDown_300_250*/ var cpro_id = "u890150"; 
+}  else {
+/*mobile_20:3，创建于2014-4-12*/  var cpro_id = "u1520137";
+   }</script> 
 
 <script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>
 <script src="http://cpro.baidu.com/cpro/ui/f.js" type="text/javascript"></script> 
