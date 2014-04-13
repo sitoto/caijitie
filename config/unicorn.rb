15aa1c542176b2e4c17d8411daa6ee1b448f745d
sitoto <huang.gadela@gmail.com>
2014-04-14 07:36:45 +0800

    a uni

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,7 +14,7 @@ pid "#{Rails.root}/tmp/pids/unicorn.pid"
 stderr_path "#{Rails.root}/log/unicorn.log"
 stdout_path "#{Rails.root}/log/unicorn.log"
 
-listen 6000, :tcp_nopush => false
+listen 5000, :tcp_nopush => false
 
 listen "/tmp/unicorn.lehazi.sock"
 worker_processes 6
