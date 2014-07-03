2f46d1dd14373f835038928e1fc9a76155d0ece5
sitoto <huang.gadela@gmail.com>
2014-07-03 22:23:24 +0800

    add douban replay

--- app/models/doubanhuati_tuoshui_job.rb
+++ app/models/doubanhuati_tuoshui_job.rb
@@ -1,7 +1,9 @@
 #encoding: UTF-8
 require 'nokogiri'
 require 'open-uri'
+require 'common'
 class DoubanhuatiTuoshuiJob
+  include Common
   def self.cai_doubanhuati_topic(url, s = 0)
     #判断是否可以访问该网址
     begin
@@ -29,7 +31,7 @@ class DoubanhuatiTuoshuiJob
     doc = Nokogiri::HTML(html_stream)
     t =  filter_doubaihuati_post(doc, topic.author,page_url.id, topic.status)
   end
- def self.filter_doubaihuati_post(doc, lz, url_id, s = 0)
+  def self.filter_doubaihuati_post(doc, lz, url_id, s = 0)
     i = 0
     j = 0
     unless doc.at_css("div.topic-doc > h3 > span.color-green").blank?
@@ -38,27 +40,42 @@ class DoubanhuatiTuoshuiJob
       firstcontent = doc.at_css("div.topic-doc  div.topic-content").inner_html
       j = j+1
       DoubanPost.create!(:page_url_id => url_id, :content => firstcontent, :post_at => created_at,
-                    :level  => 0, :my_level => j ,:author => lz)
+                         :level  => 0, :my_level => j ,:author => lz)
     end
     doc.css(".reply-doc").each_with_index do |item, i|
       author = item.at_css("a").text
       created_at  = item.at_css("h4").text
-			content= item.at_css("p").inner_html
+      content= item.at_css("p").inner_html
+
+
+      if item.at_css("div.reply-quote").blank?                                                                                                                                                          
+        content =  item.at_css("p").inner_html.to_s.strip_href_tag
+
+      else
+        replay_content = item.at_css("div.reply-quote > span.all").inner_html.to_s.strip_href_tag
+        replay_author = item.at_css("div.reply-quote > span.pubdate").inner_html.to_s.strip_href_tag
+        content = "<pre>#{replay_content} (#{replay_author})</pre> #{ item.at_css("p").inner_html.to_s.strip_href_tag}"
+
+      end
+
+
+
+
       if (s == 0 && author == lz)
         #@temp_posts[i] = [i,created_time,content,j+1]
         j += 1
         DoubanPost.create!(:page_url_id => url_id, :content => content, :post_at => created_at,
-                    :level  => i, :my_level => j ,:author => author)
+                           :level  => i, :my_level => j ,:author => author)
       elsif s == 1
         j += 1
         DoubanPost.create!(:page_url_id => url_id, :content => content, :post_at => created_at,
-                    :level  => i, :my_level => j ,:author => author)
+                           :level  => i, :my_level => j ,:author => author)
       end
     end
     return  j
-    rescue
-      return -1
- end
+  rescue
+    return -1
+  end
 
 
   def self.is_pagnation(doc)
@@ -90,9 +107,9 @@ class DoubanhuatiTuoshuiJob
     all_post_num = all_page_num.to_i * 100
 
     t = {:title => title, :classname => category, :author => lz,
-                  :firsttime => created_at,  :myupdatetime => Time.now,
-                  :mypagenum => all_page_num, :mypostnum => all_post_num,
-                  :tags => title ,  :section_id => 3,  :rule => 3, :status => s}
+      :firsttime => created_at,  :myupdatetime => Time.now,
+      :mypagenum => all_page_num, :mypostnum => all_post_num,
+      :tags => title ,  :section_id => 3,  :rule => 3, :status => s}
 
   end
 end
