73f6a21572f958709108e4f1ef36a87f3f1f45da
sitoto <huang.gadela@gmail.com>
2014-08-04 21:51:25 +0800

    tianya

--- public/js/ty_2_img.js
+++ public/js/ty_2_img.js
@@ -4,8 +4,16 @@ function ImgRebuild() {
   {
     this.src = $(this).attr("original");
     $(this).removeAttr("height");
-  }
 
+    url = this.src;
+    width = this.width;
+    height = this.height;
+    var imgid = Math.random(),
+  frameid = 'frameimg' + imgid;
+  window['img'+imgid] = '<img id="img" src=\''+url+'?kilobug\' /><script>window.onload = function() { parent.document.getElementById(\''+frameid+'\').height = document.getElementById(\'img\').height+\'px\'; }<'+'/script>';
+  img_r = '<iframe id="'+frameid+'" src="javascript:parent[\'img'+imgid+'\'];" frameBorder="0" height="' +height+ '" scrolling="no" width="100%"></iframe>';
+  $(this).replaceWith(img_r);
+  }
   });
   return '';
 }
