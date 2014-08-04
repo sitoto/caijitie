47c2262fff4d65bffbb8f6060fbbcecae7f8ace2
sitoto <huang.gadela@gmail.com>
2014-08-04 21:10:46 +0800

    add douban image shwo

---
+++ public/js/db_1_img.js
@@ -0,0 +1,15 @@
+function ImgRebuild() {
+  $(".topic-figure img").each(function(i){
+    url = this.src;
+    width = this.width;
+    height = this.height;
+    var imgid = Math.random(),
+  frameid = 'frameimg' + imgid;
+  window['img'+imgid] = '<img id="img" src=\''+url+'?kilobug\' /><script>window.onload = function() { parent.document.getElementById(\''+frameid+'\').height = document.getElementById(\'img\').height+\'px\'; }<'+'/script>';
+  img_r = '<iframe id="'+frameid+'" src="javascript:parent[\'img'+imgid+'\'];" frameBorder="0" height="' +height+ '" scrolling="no" width="100%"></iframe>';
+  $(this).replaceWith(img_r);
+  });
+  return '';
+}
+ImgRebuild();
+
