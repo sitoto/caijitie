cc8bb01228af42dd8a05e2ceb79091a00d45223c
sitoto <huang.gadela@gmail.com>
2014-10-26 08:10:47 +0800

    update to ali

--- public/js/bad_1lou.js
+++ public/js/bad_1lou.js
@@ -1,12 +1,5 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|UCWEB|MQQBrowser/i.test(navigator.userAgent) ) {
-  document.writeln("<script type=\"text/javascript\">");
-  document.writeln("var cpro_id = \"u1518435\";");
-  document.writeln("</script>");
-  document.writeln("<script src=\"http://cpro.baidustatic.com/cpro/ui/cm.js\" type=\"text/javascript\"></script>");
 }
 else {
-  document.writeln("<script type=\"text/javascript\">");
-  document.writeln("var cpro_id = \"u891731\";");
-  document.writeln("</script>");
-  document.writeln("<script src=\"http://cpro.baidustatic.com/cpro/ui/c.js\" type=\"text/javascript\"></script>");
+  document.writeln("<script charset=\"gbk\" src=\"http://p.tanx.com/ex?i=mm_29973065_7928681_26866623\"></script>");
 } 
--- public/js/bad_head.js
+++ public/js/bad_head.js
@@ -2,12 +2,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|UCWEB|MQQBrow
 
   document.writeln("<script charset=\"gbk\" src=\"http://p.tanx.com/ex?i=mm_29973065_7928681_26834330&m=1\"></script>");
 
-
-
 } else {
-  document.writeln("<script type=\"text/javascript\">");
-  document.writeln("var cpro_id = \"u891671\";");
-  document.writeln("</script>");
-  document.writeln("<script src=\"http://cpro.baidustatic.com/cpro/ui/c.js\" type=\"text/javascript\"></script>");
+  document.writeln("<script charset=\"gbk\" src=\"http://p.tanx.com/ex?i=mm_29973065_7928681_26884149\"></script>");
 
 }
