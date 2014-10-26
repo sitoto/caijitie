6a55df462b27a627f5efbaab9d0f4e6dbf49555f
sitoto <huang.gadela@gmail.com>
2014-10-26 08:58:17 +0800

    add one taobaoke

--- public/js/bad_1lou.js
+++ public/js/bad_1lou.js
@@ -1,4 +1,6 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|UCWEB|MQQBrowser/i.test(navigator.userAgent) ) {
+  document.writeln("<a href=\"http://temai.taobao.com/event471.htm?q=5Y5zG3h%2FYbVt3vqbdXnGlp6vF1dZgun72tiwRw%2BPRmQgT20PvSWKYFnFGWZjX0lL7uKdU0CgjUU%3D\" target=\"_blank\">爱在深秋，呵护相伴(全场包邮）</a>");
+
 }
 else {
   document.writeln("<script charset=\"gbk\" src=\"http://p.tanx.com/ex?i=mm_29973065_7928681_26866623\"></script>");
