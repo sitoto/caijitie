45122b6edc6475877ddf2eab4fe1a0133daa9cde
sitoto <huang.gadela@gmail.com>
2015-11-24 10:04:52 +0800

    Update bad_xuanfu.js

--- public/js/bad_xuanfu.js
+++ public/js/bad_xuanfu.js
@@ -1,5 +1,12 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|UCWEB|MQQBrowser/i.test(navigator.userAgent) ) {
+  if (Math.random() > 0.5) {
   document.writeln("<script src=\"http://s.jusha.com/?q=2087|742\"></script>");
+  } else {
+   document.writeln(" <script type=\"text/javascript\">");
+   document.writeln("var cpro_id = \"u1635580\";");
+   document.writeln("</script>");
+   document.writeln("<script src=\"http://cpro.baidustatic.com/cpro/ui/cm.js\" type=\"text/javascript\"></script>");
+  }  
 } else {
   document.writeln("<script type=\"text/javascript\">");
   document.writeln("var cpro_id = \"u890141\";");
