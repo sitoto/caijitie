02f82da56794b4ef29a9f150b633cfd3c4a349d6
sitoto <huang.gadela@gmail.com>
2015-12-15 13:54:13 +0800

    Update bad_windows.js

--- public/js/bad_windows.js
+++ public/js/bad_windows.js
@@ -3,7 +3,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|UCWEB|MQQBrow
   document.writeln("<script src=\"http://s.jusha.com/?q=2087|742\"></script>");
   } else {
    document.writeln(" <script type=\"text/javascript\">");
-   document.writeln("var cpro_id = \"u1635580\";");
+   document.writeln("var cpro_id = \"u2449949\";");
    document.writeln("</script>");
    document.writeln("<script src=\"http://cpro.baidustatic.com/cpro/ui/cm.js\" type=\"text/javascript\"></script>");
   }    
