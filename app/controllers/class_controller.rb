9fd22246926cd4b716d8f713af90b98fb0c25b51
sitoto <huang.gadela@gmail.com>
2017-11-11 16:35:47 +0800

    update sql to .uniq

--- app/controllers/class_controller.rb
+++ app/controllers/class_controller.rb
@@ -3,7 +3,8 @@ class ClassController < ApplicationController
   caches_page :index
 
   def index
-    @classes = Topic.find_by_sql("SELECT distinct(classname)  FROM `topics` ")
+#    @classes = Topic.find_by_sql("SELECT distinct(classname)  FROM `topics` ")
+    @classes = Topic.select('classname').uniq
     #@authores = Topic.find_by_sql("SELECT distinct(author)  FROM `topics`")
   end
 
